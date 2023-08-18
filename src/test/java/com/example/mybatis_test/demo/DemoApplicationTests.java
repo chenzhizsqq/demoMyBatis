9e46e4599349f2191258b8476b86efbc9ad9faff
chenzhizsqq <chenzhi@qq.com>
2023-08-18 16:56:07 +0900

    first commit

---
+++ src/test/java/com/example/mybatis_test/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatis_test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
